e6f823d0fe26fc8934ae486419361c0807c2f6ec
Юрий Бедняк <urijbednak@MacBook-Air-Urij.local>
2026-03-09 10:49:49 +0200

    make hw

--- src/models/note.js
+++ src/models/note.js
@@ -1,37 +1,18 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 export const noteSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    content: {
-      type: String,
-      default: '',
-      trim: true,
-    },
+    title: { type: String, required: true, trim: true },
+    content: { type: String, default: '', trim: true },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  },
+  { timestamps: true, versionKey: false },
 );
+noteSchema.index({ title: 'text', content: 'text' });
+
 export const Note = model('note', noteSchema);
